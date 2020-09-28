c53adca6fe633d721cc9c88b18c7b8f1560ce870
Carina <cpetravicius@gmail.com>
2020-09-28 00:00:39 -0300

    Initial Keycloak config with roles.

--- demo-keycloak-client/src/main/kotlin/com/example/demo/api/ProductApi.kt
+++ demo-keycloak-client/src/main/kotlin/com/example/demo/api/ProductApi.kt
@@ -6,19 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import java.security.Principal
+import javax.annotation.security.RolesAllowed
 
 @RequestMapping("/v1")
 @Api(tags = ["Product"])
 interface ProductApi {
 
+    @RolesAllowed("branch_office")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/product")
     fun getProduct(principal: Principal): String
 
+    @RolesAllowed("company")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/products/all")
     fun getAllProducts(principal: Principal): String
 
+    @RolesAllowed("branch_office")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/test")
     fun getMessageTest(): String
